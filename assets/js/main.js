e48e59d31d587d3212f0cf5c6e7eabf49d88ad9a
mattemutti <mattemutti@gmail.com>
2024-02-19 15:20:17 +0100

    add calc price tiket

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,19 @@
+
 // - Richiedere all'utente i km che vuole percorrere tramite un prompt 
 // - Richiedere all'utente l'età tramite un prompt
 
-const distance = Number(prompt (`Quanti km vuoi percorrere`));
-const age = Number(prompt (`Quanti anni hai`));
+const distance = Number(prompt (`Quanti km vuoi percorrere`)); // km trasfirmati in numero
+const age = Number(prompt (`Quanti anni hai`)); // anni trasformati i nnumero
 
-console.log(typeof distance);
-console.log(typeof age);
 
+console.log(typeof distance, distance); // Controllo e stampa in console tipo del dato e numero digitato
+console.log(typeof age, age);
 
 
 // - calcolare il costo del biglietto in base ai km di percorrenza
+
+let price = Number((distance * 0.21)) // calcolo costo biglietto in base ai km
+console.log(typeof price, price);
 
 
 // - controllare se il prezzo deve essere soggetto a scontistica in base all'età dell'utente
